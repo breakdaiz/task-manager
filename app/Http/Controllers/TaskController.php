83a1017e1b65d073f70685b88c70a6a7e4ae909d
bernard rayoso <rayosobernard@gmail.com>
2023-07-26 02:55:09 +0800

    Allow sorting, filters and pagination

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,14 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
 use App\Http\Resources\TaskCollection;
 use App\Http\Resources\TaskResource;
 use App\Models\Task;
 use Illuminate\Http\Request;
+use Spatie\QueryBuilder\QueryBuilder;
 class TaskController extends Controller
 {       
     public function index(Request $request) {
-        return new TaskCollection(Task::all()); 
+
+    $tasks = QueryBuilder::for(Task::class)
+        ->allowedFilters('is_done')
+        ->defaultSort('-created_at')
+        ->allowedSorts(['title', 'is_done', 'created_at'])
+        ->paginate();
+
+    return new TaskCollection($tasks);
+
+#        return new TaskCollection(Task::paginate()); 
     }
 
     public function show(Request $request, Task $task) {
@@ -18,10 +29,21 @@ class TaskController extends Controller
     }
     
     public function store(StoreTaskRequest $request) {
-        $validated =$request->validated();
+        $validated = $request->validated();
         $task = Task::create($validated);
         return new TaskResource($task);
     }
 
-    
+    public function update(UpdateTaskRequest $request,Task $task)
+    {
+        $validated = $request-> validated();
+        $task->update($validated);
+        return new TaskResource($task);
+    }
+
+    public function destroy(Request $request, Task $task)
+    {
+        $task->delete();
+        return response()->noContent();
+    }
 }
