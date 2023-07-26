9d7382258353777a233ec8d88995043649cf5047
bernard rayoso <rayosobernard@gmail.com>
2023-07-26 14:00:10 +0800

    add  register feature

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -29,14 +29,16 @@ class AuthController extends Controller
 
     // Register
     public function register(Request $request){
+
         $validated = $request->validate([
-        'name' => 'required|max:255',
-        'email' => 'required|max:255|email|unique:users, email',
+        'name' =>  'required|max:255',
+        'email'=> 'required|max:255|email|unique:users,email',
         'password' => 'required|confirmed|min:6'
       ]);
 
 
-      $user = User::create($validated); 
+       $user = User::create($validated); 
+    
       return response()->json([
         'data' => $user,
         'access_token' => $user->createToken('api_token') ->plainTextToken,
